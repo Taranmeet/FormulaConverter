54a7d63afbf0fdaaccf61fc802d0d3dfd44aaa86
Taranmeet <mr.taranmeet@gmail.com>
2021-03-29 15:54:20 +0530

    Worked on last api to get formula to render on sample UI Added Picasso support library to render image on UI

--- app/src/main/java/com/tex/FormulaActivity.java
+++ app/src/main/java/com/tex/FormulaActivity.java
@@ -3,7 +3,10 @@ package com.tex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 import com.tex.viewmodel.FormulaViewModel;
 
 public class FormulaActivity extends AppCompatActivity {
@@ -16,7 +19,19 @@ public class FormulaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formula);
         viewModel = new FormulaViewModel();
         findViewById(R.id.tv_go).setOnClickListener(v -> {
-            viewModel.onClick();
+            viewModel.onClick().observe(this, s -> {
+                Picasso.get().load("https://en.wikipedia.org/api/rest_v1/media/math/render/png/" + s).into(findViewById(R.id.iv_go), new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Log.e("TARAN", "Image loaded Success !!");
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("TARAN", "Image loaded Failure !!");
+                    }
+                });
+            });
         });
     }
 }
--- app/src/main/java/com/tex/viewmodel/FormulaViewModel.java
+++ app/src/main/java/com/tex/viewmodel/FormulaViewModel.java
@@ -2,6 +2,7 @@ package com.tex.viewmodel;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.tex.network.NetworkRepo;
@@ -16,13 +17,17 @@ import retrofit2.Response;
 
 public class FormulaViewModel extends ViewModel {
 
-    public void onClick() {
+    public MutableLiveData<String> hash = new MutableLiveData<>();
+
+    public MutableLiveData<String> onClick() {
         NetworkRepo.build().create(WikiService.class).checkExpression(RequestBody.create("a + b = c", MediaType.parse("multipart/form-data"))).enqueue(new Callback<WikiResponse>() {
             @Override
             public void onResponse(Call<WikiResponse> call, Response<WikiResponse> response) {
                 if (response != null) {
                     if (response.isSuccessful()) {
                         Log.e("TARAN", "Success " + response.body());
+                        String hashString = response.headers().get("x-resource-location");
+                        hash.postValue(hashString);
                     } else {
                         Log.e("TARAN", "Failure " + response.code());
                     }
@@ -34,6 +39,7 @@ public class FormulaViewModel extends ViewModel {
                 Log.e("TARAN", "Fail");
             }
         });
+        return hash;
     }
 
 }
