3b2f9a0e8e364ae45d3be3a3bcdc996e987e1686
Taranmeet <mr.taranmeet@gmail.com>
2021-04-04 14:55:59 +0530

    Added DataBinding to formula activity.

--- app/src/main/java/com/tex/view/FormulaActivity.java
+++ app/src/main/java/com/tex/view/FormulaActivity.java
@@ -4,49 +4,57 @@ import android.Manifest;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.tex.R;
 import com.tex.base.BaseActivity;
+import com.tex.databinding.FormulaActivityBinding;
 import com.tex.viewmodel.FormulaViewModel;
 
 import java.io.File;
 
 public class FormulaActivity extends BaseActivity {
 
-    private FormulaViewModel viewModel;
+    /**
+     * View model for formula activity.
+     */
+    private FormulaViewModel mViewModel;
+
+    /**
+     * Binding for formula UI.
+     */
+    private FormulaActivityBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formula);
-        viewModel = new FormulaViewModel();
+        mBinding = DataBindingUtil.setContentView(this, R.layout.formula_activity);
+        mViewModel = new FormulaViewModel();
 
         checkPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE, () -> {
-            viewModel.setCachingEnabled(true);
+            mViewModel.setCachingEnabled(true);
         }, () -> {
-            viewModel.setCachingEnabled(true);
+            mViewModel.setCachingEnabled(true);
             Toast.makeText(this, "Caching of images will not work indefinitely", Toast.LENGTH_SHORT).show();
         });
 
-        File f = new File(getExternalFilesDir(null) + File.separator + "Future Studio Icon.png");
-        findViewById(R.id.tv_go).setOnClickListener(v -> {
-            viewModel.checkFormula().observe(this, checkFormulaObserver);
+        mBinding.tvGo.setOnClickListener(v -> {
+            mViewModel.checkFormula().observe(this, checkFormulaObserver);
         }
         );
 
     }
 
-    Observer<String> checkFormulaObserver = new Observer<String>() {
+    private final Observer<String> checkFormulaObserver = new Observer<String>() {
         @Override
         public void onChanged(String s) {
             File file = new File(s);
-            Picasso.get().load(Uri.fromFile(file)).into(findViewById(R.id.iv_go), new Callback() {
+            Picasso.get().load(Uri.fromFile(file)).into(mBinding.ivGo, new Callback() {
                 @Override
                 public void onSuccess() {
                     Log.e("Picaso", "Success");
