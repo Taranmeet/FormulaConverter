6324475fd3ebdea6e31b413c5db355e14b7d2a11
Taranmeet <mr.taranmeet@gmail.com>
2021-04-05 09:39:31 +0530

    Added comments to utils file

--- app/src/main/java/com/tex/utils/Utils.java
+++ app/src/main/java/com/tex/utils/Utils.java
@@ -14,15 +14,33 @@ import okhttp3.ResponseBody;
 
 public class Utils {
 
+    /**
+     * Method to remove spaces and white space chars from string.
+     *
+     * @param input Raw input with spaces.
+     * @return String with spaces removed.
+     */
     public static String trimWhiteSpaces(String input) {
         return input.replaceAll("\\s", "");
     }
 
+    /**
+     * Method to check if image is already cached in local storage.
+     *
+     * @param iHash hash of image stored.
+     * @return true if image is already present.
+     */
     public static boolean isImageCached(String iHash) {
         File file = new File(getImagePath(iHash));
         return file.exists();
     }
 
+    /**
+     * Method to get image path from hash string.
+     *
+     * @param iHash String hash from server.
+     * @return Path  for file to be saved.
+     */
     public static String getImagePath(String iHash) {
         return FormulaApplication.mContext.getExternalFilesDir(null)
                 + File.separator
@@ -30,9 +48,15 @@ public class Utils {
                 + ".png";
     }
 
+    /**
+     * Method to write Image in response body to disk.
+     *
+     * @param body Response body from server.
+     * @param file file to write data to.
+     * @return true when write was success false otherwise.
+     */
     public static boolean writeResponseBodyToDisk(ResponseBody body, File file) {
         try {
-
             if (!file.exists()) {
                 boolean created = file.createNewFile();
                 Log.e("Taran", "created " + created);
@@ -43,7 +67,6 @@ public class Utils {
 
             try {
                 byte[] fileReader = new byte[4096];
-
                 long fileSize = body.contentLength();
                 long fileSizeDownloaded = 0;
 
@@ -52,20 +75,14 @@ public class Utils {
 
                 while (true) {
                     int read = inputStream.read(fileReader);
-
                     if (read == -1) {
                         break;
                     }
-
                     outputStream.write(fileReader, 0, read);
-
                     fileSizeDownloaded += read;
-
                     Log.d("TAG", "file download: " + fileSizeDownloaded + " of " + fileSize);
                 }
-
                 outputStream.flush();
-
                 return true;
             } catch (IOException e) {
                 return false;
@@ -73,7 +90,6 @@ public class Utils {
                 if (inputStream != null) {
                     inputStream.close();
                 }
-
                 if (outputStream != null) {
                     outputStream.close();
                 }
