2aa4bdde0a060f43b4b63d8fb7a7a35ca8c9499f
Taranmeet <mr.taranmeet@gmail.com>
2021-04-05 10:08:37 +0530

    Worked on image share scenerio when cache permission not provided

--- app/src/main/java/com/tex/viewmodel/FormulaViewModel.java
+++ app/src/main/java/com/tex/viewmodel/FormulaViewModel.java
@@ -19,51 +19,79 @@ import java.io.File;
 
 public class FormulaViewModel extends ViewModel {
 
+    /**
+     * Live data to observe check formula responses.
+     */
     public MutableLiveData<Uri> mImageLiveData = new MutableLiveData<>();
 
+    /**
+     * Live data to observe when cache permission is not granted and we directly call server using picasso.
+     */
     public MutableLiveData<String> mImageDirectAccessLiveData = new MutableLiveData<>();
 
+    /**
+     * Live data to observe for error scenarios
+     */
     public MutableLiveData<FormulaModel> mErrorModel = new MutableLiveData<>();
 
+    /**
+     * Boolean providing situational awareness for cache related permissions.
+     */
     private boolean isCachingEnabled = false;
 
+    /**
+     * Uri of latest loaded image.
+     */
     private Uri mImageUri = null;
 
+    /**
+     * Url of latest loaded image available when cache is not present.
+     */
+    private String mImageURL = null;
+
+    /**
+     * Method to check if formula is in tex format then download its image from servers.
+     *
+     * @param iQuery Formula to be checked.
+     * @return Live data to be monitored for server response.
+     */
     public MutableLiveData<Uri> checkFormula(String iQuery) {
-            DataRepo.checkFormula(Utils.trimWhiteSpaces(iQuery)).observeForever(formulaModel -> {
-                if (formulaModel != null && formulaModel.mSuccess) {
-                    String hashString = formulaModel.mFormulaHash;
-                    if (isCachingEnabled) {
-                        DataRepo.downloadImage(hashString).observeForever(s -> {
-                            if (formulaModel.mSuccess) {
-                                DBRepoFactory.getInstance().saveFormulaToDb(formulaModel);
-                            }
-                            File file = new File(s);
-                            mImageUri = Uri.fromFile(file);
-                            mImageLiveData.postValue(mImageUri);
-                        });
-                    } else {
-                        mImageDirectAccessLiveData.postValue(ProjectConstants.BASE_IMAGE_URL + hashString);
-                    }
-                } else if (formulaModel != null && !formulaModel.isInternetIssue) {
-                    formulaModel.isBadRequest = true;
-                    mErrorModel.postValue(formulaModel);
-                } else if (formulaModel != null && formulaModel.isInternetIssue) {
-                    mErrorModel.postValue(formulaModel);
-                } else{
-                    FormulaModel failModel = new FormulaModel();
-                    failModel.mFormula = Utils.trimWhiteSpaces(iQuery);
-                    failModel.isBadRequest = true;
-                    mErrorModel.postValue(failModel);
+        DataRepo.checkFormula(Utils.trimWhiteSpaces(iQuery)).observeForever(formulaModel -> {
+            if (formulaModel != null && formulaModel.mSuccess) {
+                String hashString = formulaModel.mFormulaHash;
+                if (isCachingEnabled) {
+                    DataRepo.downloadImage(hashString).observeForever(s -> {
+                        if (formulaModel.mSuccess) {
+                            DBRepoFactory.getInstance().saveFormulaToDb(formulaModel);
+                        }
+                        File file = new File(s);
+                        mImageUri = Uri.fromFile(file);
+                        mImageLiveData.postValue(mImageUri);
+                    });
+                } else {
+                    mImageURL = ProjectConstants.BASE_IMAGE_URL + hashString;
+                    mImageDirectAccessLiveData.postValue(mImageURL);
                 }
-            });
+            } else if (formulaModel != null && !formulaModel.isInternetIssue) {
+                formulaModel.isBadRequest = true;
+                mErrorModel.postValue(formulaModel);
+            } else if (formulaModel != null && formulaModel.isInternetIssue) {
+                mErrorModel.postValue(formulaModel);
+            } else {
+                FormulaModel failModel = new FormulaModel();
+                failModel.mFormula = Utils.trimWhiteSpaces(iQuery);
+                failModel.isBadRequest = true;
+                mErrorModel.postValue(failModel);
+            }
+        });
         return mImageLiveData;
     }
 
-    public boolean isCachingEnabled() {
-        return isCachingEnabled;
-    }
-
+    /**
+     * Method to update view model if cache can be enabled.
+     *
+     * @param cachingEnabled true when permission is granted
+     */
     public void setCachingEnabled(boolean cachingEnabled) {
         isCachingEnabled = cachingEnabled;
     }
@@ -74,11 +102,19 @@ public class FormulaViewModel extends ViewModel {
      * @return Intent created for share image.
      */
     public Intent shareImage() {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_STREAM, mImageUri);
-        sendIntent.setType("image/png");
-        return Intent.createChooser(sendIntent, "Send your image");
+        if (isCachingEnabled) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_STREAM, mImageUri);
+            sendIntent.setType("image/png");
+            return Intent.createChooser(sendIntent, "Send your image");
+        } else {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+            sendIntent.setType("image/png");
+            return Intent.createChooser(sendIntent, "Send your image");
+        }
     }
 
     /**
